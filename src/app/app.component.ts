11973fed04c9059612ed5eeb420df4a6aeab518d
Tanya627 <tanyaocampo96@yahoo.com>
2022-08-11 14:15:34 -0500

    - Adding state to our app.component - added functionality on preess of the ADD button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
   title = 'teamapp1';
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
+  addMember() {
+    // the if ! is checking for a falsy value, which is " "
+    if (!this.newMemberName) {
+      this.errorMessage = 'Your name cant be empty 👎';
+      return;
+    }
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
 }
