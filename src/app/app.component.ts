0b344559f12a563747e993cd3ec47789c3991edb
Tanya627 <tanyaocampo96@yahoo.com>
2022-08-13 20:17:28 -0500

    - added fucntionality to the buttons - built the tema generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
   members: string[] = [];
   errorMessage = '';
   title = 'teamapp1';
-
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
   onInput(member: string) {
     this.newMemberName = member;
   }
@@ -23,5 +24,31 @@ export class AppComponent {
     this.errorMessage = '';
     this.members.push(this.newMemberName);
     this.newMemberName = '';
+  }
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
+  }
+  generateTeams() {
+    if (this.members.length < this.numberOfTeams) {
+      this.errorMessage = 'Not enough memebers';
+    }
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = 'Invalid number of teams';
+    }
+    const allMemebrs = [...this.members];
+    while (allMemebrs.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMemebrs.length);
+        const member = allMemebrs.splice(randomIndex, 1)[0];
+        if (!member) break;
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+    this.members = [];
+    this.numberOfTeams = '';
   }
 }
